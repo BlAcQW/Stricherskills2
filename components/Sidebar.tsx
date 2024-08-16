1a1d2f7abde2e5fea901d66f372b87ce14363914
BlAcQW <enochhenyo@gmail.com>
2024-08-16 16:04:49 +0000

    Navbar and MobileNav done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,9 +11,9 @@ const Sidebar = () => {
     const pathname =usePathname ();
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
-        <div className='flex flex- flex-col gap-6'>
+        <div className='flex flex-1 flex-col gap-6'>
             {SidebarLinks.map((link) =>{
-                const IsActive =  pathname === link.route || pathname.startsWith(link.route);   //checking if pathename is active and if the route is same
+                const IsActive =  pathname === link.route || pathname.startsWith(`${link.route}/`);   //checking if pathename is active and if the route is same
                  return(
                     <Link href={link.route} key={link.route} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',
                         {"bg-blue-1":IsActive}
