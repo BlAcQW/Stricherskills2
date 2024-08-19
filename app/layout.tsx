3d3309105e2643df7be343b08b519d347a7ef975
BlAcQW <enochhenyo@gmail.com>
2024-08-19 16:55:18 +0000

    auth Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Variable } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ClerkProvider 
+      appearance={{
+        variables : {
+          colorText :'#fff',
+          colorPrimary :'#0E78F9',
+          colorBackground :'#1C1F2E',
+          colorInputBackground :'#252A41',
+          colorInputText :'#fff'
+
+        }
+
+        
+      }}>
       <body className={`${inter.className} bg-dark-2`}>{children}</body>
+      </ClerkProvider>
     </html>
   );
 }
