1c5c65595fbc5896271a8e30cdfe66875b4a98d3
BlAcQW <enochhenyo@gmail.com>
2024-08-21 15:33:12 +0000

    stream comfig done

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,11 +4,60 @@ import { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
 import MeetingModel from './MeetingModel'
+import { useUser } from '@clerk/nextjs'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { useToast } from "@/components/ui/use-toast"
+
 
 const MeetingTypeList = () => {
-    const creaeMeeting = () => { }
+    
     const router = useRouter();
-    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>();
+    const [values,setValues] = useState({
+        dateTime:new Date(),
+       description :"",
+       link:"" 
+    })
+    const [callDetails, setCallDetails] = useState<Call>();
+    const { toast } = useToast();
+    const {user} =useUser();
+    const client =useStreamVideoClient();
+    const creaeMeeting = async() => { 
+        if(!client||!user) return;
+        try {
+            if(!values.dateTime){
+                toast({title: "Please select Date and Time", })
+                return;
+            }
+            const id =crypto.randomUUID();
+            const call=client.call('default' ,id)
+            
+            if(!call) throw new  Error('falled to create call')
+
+             const startAt=   values.dateTime.toISOString() ||new Date(Date.now()).toISOString();
+             const description =values.description || "instant Meeting";
+
+             await call.getOrCreate({
+                data :{
+                    starts_at:startAt,
+                    custom :{
+                        description
+                    }
+                }
+             })
+             setCallDetails(call);
+             if(!values.description){
+                router.push(`/meeting/${call.id}`)
+             }
+             toast({title: "Meeting Created", })
+        } catch (error) {
+            console.log(error);
+            toast({
+                title: "Failed to create Meeting",
+                
+              })
+        }
+    }
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
             <HomeCard
