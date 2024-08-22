ff9b4a6137384261708014c89ef353ea4aa39cdb
BlAcQW <enochhenyo@gmail.com>
2024-08-22 16:18:12 +0000

    meeting setup done

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,33 @@
+import { PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import {useState} from 'react'
+
+type CallLayoutType = 'grid' |'speaker-left' | 'speaker-right'
+
+const MeetingRoom = () => {
+
+const [layout, setlayout] = useState<CallLayoutType>('speaker-left')
+
+const CallLayout =() =>{
+  switch (layout) {
+    case 'grid':
+      return <PaginatedGridLayout/>
+    case 'speaker-right':
+      return <SpeakerLayout participantsBarPosition='left'/>
+     default :
+        return <SpeakerLayout participantsBarPosition='right'/>  
+    
+  }
+}
+  return (
+    <section className='relative h-full w-full overflow-hidden pt-4 text-white'>
+      <div className='relative flex size-full items-center justify-center'>
+        <div className='flex size-full max-w-[1000px] items-center '>
+
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default MeetingRoom
