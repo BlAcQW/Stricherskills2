884dee8e7db23ee59ab458d754c1e8c7bfd8b768
BlAcQW <enochhenyo@gmail.com>
2024-08-23 17:20:37 +0000

    still on meeting room

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,11 +1,15 @@
-import { PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import { cn } from '@/lib/utils'
+import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
 import {useState} from 'react'
-
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { LayoutList } from 'lucide-react'
 type CallLayoutType = 'grid' |'speaker-left' | 'speaker-right'
 
 const MeetingRoom = () => {
 
 const [layout, setlayout] = useState<CallLayoutType>('speaker-left')
+
+const [showParticipant, setShowParticipant] = useState(false)
 
 const CallLayout =() =>{
   switch (layout) {
@@ -23,8 +27,43 @@ const CallLayout =() =>{
       <div className='relative flex size-full items-center justify-center'>
         <div className='flex size-full max-w-[1000px] items-center '>
 
-
+        <CallLayout/>
         </div>
+        <div className={cn('h-[calc(100vh-86px)] hidden ml-2',{'show-block':showParticipant})}>
+          <CallParticipantsList onClose={() => {setShowParticipant(false)}}/>
+        </div>
+      </div>
+      <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
+        <CallControls />
+
+        {/* <Menu>
+          <div className='flex items-center'>
+          <MenuButton className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+            <LayoutList size={20} className='text-white'/>
+            </MenuButton>
+          </div>
+      
+      <MenuItems anchor="bottom" className='border-dark-1 bg-dark-1 text-white'>
+        {['Grid','Speaker-Left' ,'Speaker-Right'].
+        map((item,index) =>(
+            <div key={index}>
+                <MenuItem>
+                {item}
+                </MenuItem>
+            </div>
+        ))}
+      </MenuItems>
+    </Menu> */}
+     <Menu>
+     <div className='flex items-center'>
+          <MenuButton className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+            <LayoutList size={20} className='text-white'/>
+            </MenuButton>
+          </div>
+      <MenuItems anchor="bottom" className='border-dark-1 bg-dark-1 text-white'>
+          
+      </MenuItems>
+    </Menu>
       </div>
     </section>
   )
